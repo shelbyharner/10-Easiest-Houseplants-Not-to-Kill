f83707266d71505935962376433ce74683f79f75
Shelby Harner <shelby.harner@gmail.com>
2020-12-16 21:43:45 -0800

    remove unneeded function for nav bar

--- app.js
+++ app.js
@@ -71,13 +71,5 @@ function promptUser(alertMessage) {
 
 promptUser(response);
 
-//Add highlight hover over color change to nav bar
-function navAction() {
-    var navHighlight = document.getElementById("navHighlight");
-    if (navHighlight.className === "active") {
-        navHighlight.className += "responsive";
-    } else {
-        navHighlight.className = "topnav";
-    }
-}
+
 
